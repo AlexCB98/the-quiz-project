b895e7213dee769b81a5cc1c10464adf29dcb775
Alexandru <lexyus22@gmail.com>
2026-05-12 10:12:23 +0200

    Added QuizBrain class and question display logic.

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ for quest in question_data:
     new_question = Question(questions_text, questions_answer)
     questions.append(new_question)
 
+class QuizBrain:
+
+    def __init__(self, q_list):
+
+        self.question_number = 0
+        self.question_list = q_list
+
+    def show_questions(self):
+
+        current_question = self.question_list[self.question_number]
+        answer = input(f'Q.{self.question_number}: {current_question.text} (True / False)?: ')
