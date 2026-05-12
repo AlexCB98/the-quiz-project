f460d4a9d751846affd0b53cc56db528d0087e6f
Alexandru <lexyus22@gmail.com>
2026-05-12 09:27:36 +0200

    Setup quiz project files and question data.

---
+++ data.py
@@ -0,0 +1,21 @@
+question_data = [
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+    {"text": "A few ounces of chocolate can kill a small dog.", "answer": "True"},
+    {"text": "Bananas are berries, but strawberries are not.", "answer": "True"},
+    {"text": "The Great Wall of China is visible from space with the naked eye.", "answer": "False"},
+    {"text": "Sharks existed before trees.", "answer": "True"},
+    {"text": "Lightning never strikes the same place twice.", "answer": "False"},
+    {"text": "Octopuses have three hearts.", "answer": "True"},
+    {"text": "Humans can breathe and swallow at the same time.", "answer": "False"},
+    {"text": "The Eiffel Tower can grow taller during hot weather.", "answer": "True"},
+    {"text": "Goldfish only have a memory of three seconds.", "answer": "False"},
+    {"text": "There are more stars in the universe than grains of sand on Earth.", "answer": "True"},
+    {"text": "An ostrich's eye is bigger than its brain.", "answer": "True"}
+]
